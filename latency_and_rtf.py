76aa41b09b57377a1ee08526f9b1a781d3fecd80
harunori_kawano <k.hnori0507@gmail.com>
2026-03-24 19:15:27 +1100

    first commit

--- latency_and_rtf.py
+++ latency_and_rtf.py
@@ -88,11 +88,11 @@ if __name__ == "__main__":
 
     duration = 10
 
-    dummy = torch.rand(1,1,  16000 * duration)
+    dummy = torch.rand(1,  16000 * duration)
     input_lengths = torch.tensor([16000 * duration])
 
     measure_cpu_latency_and_rtf(
-        model=vit,
+        model=wav2vec2,
         inputs=(dummy,),
         audio_duration_sec=duration,
         num_warmup=10,
